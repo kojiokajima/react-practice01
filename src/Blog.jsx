b1d40401662eec3ba18006444f765ece4ff078de
wanko <kouji124@w4.dion.ne.jp>
2021-01-12 14:07:54 -0800

    modified some functions using hooks

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,61 +1,44 @@
-import React from 'react';
-import Article from './Article'
+import React from "react";
+import Article from "./Article";
 import * as FooBar from "./components/FooBar";
 import Hoge from "./components/Hoge";
 
-class Blog extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isPublished: false,
-            count: 0
-        }
-    }
+const Blog = () => {
+  // componentDidMount() {
+  //     document.getElementById("counter").addEventListener('click', this.countUp)
+  // }
 
-    componentDidMount() {
-        document.getElementById("counter").addEventListener('click', this.countUp)
-    }
+  // componentDidUpdate() {
+  //     if(this.state.count >= 10) {
+  //         this.setState({
+  //             count: 0
+  //         })
+  //     }
+  // }
 
-    componentDidUpdate() {
-        if(this.state.count >= 10) {
-            this.setState({
-                count: 0
-            })
-        }
-    }
+  // componentWillUnmount() {
+  //     document.getElementById("counter").removeEventListener('click', this.countUp)
 
-    componentWillUnmount() {
-        document.getElementById("counter").removeEventListener('click', this.countUp)
+  // }
 
-    }
 
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-    };
+  // countUp = () => {
+  //     this.setState({
+  //         count: this.state.count + 1
+  //     })
+  // }
 
-    countUp = () => {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+  return (
+    <>
+      <Article
+        title={"React"}
+        // count={this.state.count}
+      />
+      <FooBar.Foo />
+      <FooBar.Bar />
+      <Hoge />
+    </>
+  );
+};
 
-    render() {
-        return(
-            <>
-                <Article
-                    title={"React"}
-                    isPublished={this.state.isPublished}
-                    toggle={() => this.togglePublished()}
-                    count={this.state.count}
-                />
-                <FooBar.Foo />
-                <FooBar.Bar />
-                <Hoge />
-            </>
-        )
-    }
-}
-
-export default Blog
+export default Blog;
